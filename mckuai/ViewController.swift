45ce33c18d47fd15cd61b4cdc77c81f545bc18a4
xingfuqiu <xingfuqiu@gmail.com>
2014-11-01 00:54:58 +0800

    test modify file

--- mckuai/ViewController.swift
+++ mckuai/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("hello mckuai")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
