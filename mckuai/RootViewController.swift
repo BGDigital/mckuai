334a18dae564a8d2f0cef296766e90b3061eb113
xingfuqiu <xingfuqiu@gmail.com>
2015-01-04 15:53:00 +0800

    将之前的两个登录界面改为固定一个，解决chenqiang的tabBar跳转的问题

--- mckuai/RootViewController.swift
+++ mckuai/RootViewController.swift
@@ -16,24 +16,26 @@ let tencentAppKey = "101155101"
 var appUserIdSave=NSUserDefaults.standardUserDefaults().objectForKey("userId") as Int!
 var loginView:McLogin!
 var loginViewOld:Login!
+var selectItemTag = 0
 var _lastSelectedIndex = 0
 
 var selectedIndex = 0
-class RootViewController: UITabBarController{
+class RootViewController: UITabBarController, UITabBarControllerDelegate{
     
     //let launchImgUrl = "http://pic2.zhimg.com/cf3bcf3ca5c7a503e7b58d0f498f14bc.jpg"
 
     var welcome:UIViewController!
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegate = self
         //修改tabbar的选中颜色
         tabBar.selectedImageTintColor = UIColor(red: 0.224, green: 0.749, blue: 0.361, alpha: 1.00)
         //判断是否登录
         if(appUserIdSave == nil){
-            loginViewOld=UIStoryboard(name:"Login",bundle:nil).instantiateViewControllerWithIdentifier("login") as Login
-            self.view.addSubview(loginViewOld.view)
+//            loginViewOld=UIStoryboard(name:"Login",bundle:nil).instantiateViewControllerWithIdentifier("login") as Login
+//            self.view.addSubview(loginViewOld.view)
 
-//           McLogin.showLoginView(self)
+           McLogin.showLoginView(self)
         }else {
             println(appUserIdSave)
         }
@@ -45,27 +47,16 @@ class RootViewController: UITabBarController{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-//    
-//    func alertView(alertView: UIAlertView,clickedButtonAtIndex buttonIndex: Int) {
-//        if( buttonIndex==0 ) {
-//            loginView=UIStoryboard(name:"Login",bundle:nil).instantiateViewControllerWithIdentifier("login") as UIViewController
-//            self.view.addSubview(loginView.view
-//        }
-//    }
     
      override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem!) {
+        selectItemTag = item.tag
         if(item.tag == 3 && appUserIdSave == nil){
-//            McLogin.showLoginView(self)
-//              self.view.window?.rootViewController?.presentViewController(moreViewController, animated: false, completion: nil)
+            McLogin.showLoginView(self)
         }
-        
-        
-
      }
 
-
-//    override func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
-//        println("shouldSelectViewController")
-//    }
-//    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        //返回false不变，返回true要变
+        return !(selectItemTag == 3 && appUserIdSave == nil)
+    }
 }
